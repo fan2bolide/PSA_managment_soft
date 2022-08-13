09c36c5cab2c070f8d36b203d24aa9454a729e22
fan2bolide <82985749+fan2bolide@users.noreply.github.com>
2022-08-14 01:46:28 +0200

    created structure of function main

--- main.c
+++ main.c
@@ -5,7 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bajeanno <bajeanno@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/14 01:36:54 by bajeanno          #+#    #+#             */
-/*   Updated: 2022/08/14 01:36:54 by bajeanno         ###   ########lyon.fr   */
+/*   Created: 2022/08/14 01:38:46 by bajeanno          #+#    #+#             */
+/*   Updated: 2022/08/14 01:38:46 by bajeanno         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    void *datas;
+
+    create_data_structure();
+    launch_app();
+    save_data();
+    free_datas(datas);
+    return (0);
+}
